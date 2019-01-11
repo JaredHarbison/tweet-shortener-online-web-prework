326f036556008a1bace81f88f55591aaeef7f721
Jared Harbison <jared.harbison@gmail.com>
2019-01-11 15:33:22 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,13 +1,10 @@
-def dictionary
-dictionary = {
-  "too" => "2",
-  "to" => "2",
-  "two" =>"2",
-  "four" => "4",
-  "for" => "4",
-  "be" => "b",
-  "you" => "u",
-  "at" => "@",
-  "and" => "&"
-}
-end
+def dictionary(word)
+  dictionary = {
+    "hi" => "hello",
+    '2' => ["to", "two", "too"],
+    '4' => ["for", "four"],
+    'b' => "be",
+    'u' => "you",
+    "@" => "at",
+    "&" => "and"
+  }
